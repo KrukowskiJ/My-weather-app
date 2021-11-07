185266cd110f19865902742e2c0e8debb1def9ef
KrukowskiJ <jakub.krukowski@student.wat.edu.pl>
2021-11-07 19:16:46 +0100

    make open weather api connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import axios from "axios"
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const savePositionToState = (position) => {
+    setX(position.coords.latitude);
+    setY(position.coords.longitude);
+  }
+
+  const fetchWeather = async() => {
+try{
+
+  window.navigator.geolocation.getCurrentPosition(savePositionToState);
+  const res = await axios.get(
+    "https://api.openweathermap.org/data/2.5/weather?q=Lublin&appid=1039cf2e872c4c85f955bf923ace09ea&units=metric"
+    );
+    console.log(res.data);
+
+    const res1 = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${x}&lon=${y}&appid=1039cf2e872c4c85f955bf923ace09ea&units=metric`
+      );
+      console.log(res1.data);
+}catch(err)
+{
+  console.log(err);
+}
+
+
+  }
+  useEffect(() => {
+    fetchWeather();
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+       
     </div>
   );
 }
